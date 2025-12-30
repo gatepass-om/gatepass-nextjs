f03e7427fbf680e315087ab083c3743104b337a2
tamer <tamer.abbasher@gmail.com>
2025-12-30 09:08:36 +0000

    55ca80a5c8d974b9.css:1  Failed to load resource: the server responded wi

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  trailingSlash: true,
   experimental: {
     serverActions: {
       allowedOrigins: ['localhost:3000', '*.devtunnels.ms', "*.gatepass.om"],
